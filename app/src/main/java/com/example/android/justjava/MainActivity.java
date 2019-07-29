7dab8d87edb9f7dd469afd075814c2696431f476
jamesyothers <jamesjsdev@gmail.com>
2019-07-29 15:58:33 -0700

    add message to cost field and other visual updates

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -29,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice (quantity * 5);
+        int price = quantity * 5;
+        String priceMessage="Total: $" + price + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -54,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText("$" + number);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 }
