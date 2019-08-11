7ed09193d7a3bf8e78941ea2387864ff1cb818ed
jamesyothers <jamesjsdev@gmail.com>
2019-08-11 16:37:51 -0500

    add displayQuantity methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,10 +7,12 @@
  **/
 
 package com.example.android.justjava;
+
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * This app displays an order form to order coffee.
@@ -30,32 +32,27 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         int price = quantity * 5;
-        String priceMessage="Total: $" + price + "\nThank you!";
+        String priceMessage = "Total: $" + price + "\nThank you!";
         displayMessage(priceMessage);
     }
 
     public void increment(View view) {
-       quantity = quantity + 1;
-       display(quantity);
+        quantity = quantity + 1;
+        displayQuantity(quantity);
 
     }
 
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("$" + number);
     }
 
     /**
